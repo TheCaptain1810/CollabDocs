586ee5e19628b6e2a4888bc7bcacee2f381f0744
Pranav Pise <itspranavpise@gmail.com>
2025-01-16 13:58:17 +0530

    not sure about this one

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { clerkClient } from "@clerk/clerk-sdk-node";
+import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
 import { liveblocks } from "../liveblocks";
 
